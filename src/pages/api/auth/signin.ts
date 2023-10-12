a9bee5d18b63ea23528d23a90040c3199cf286bf
Robert Willie <rpwillie@gmail.com>
2023-10-12 22:53:33 +0100

    added env var to signin.ts

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -15,7 +15,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
         redirectTo: import.meta.env.DEV
           ? "http://localhost:4321/api/auth/github"
           : // Change this to your production URL
-            "https://astro-supabase-auth.vercel.app/api/auth/github",
+          import.meta.env.VERCEL_URL + "/api/auth/github",
       },
     });
 
