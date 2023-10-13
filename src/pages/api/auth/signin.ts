d084dd7cf21da5822d9a3a82f2bcd3e0eb40da5d
Robert Willie <rpwillie@gmail.com>
2023-10-13 07:43:40 +0100

    testing vercel out

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -13,10 +13,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: process.env.VERCEL_ENV === "development"
-          ? "http://localhost:4321/api/auth/github"
-          : // Change this to your production URL
-          "https://" + process.env.VERCEL_URL + "/api/auth/github",
+        redirectTo: "https://" + process.env.VERCEL_URL + "/api/auth/github",
       },
     });
 
